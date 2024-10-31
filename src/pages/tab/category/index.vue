ac24cb97c838de18298c5fb83068817c8beface2
cuilong <cuilong@AppledeMBP.lan>
2024-11-01 02:31:30 +0800

    feat: 适配数据库云函数

--- src/pages/tab/category/index.vue
+++ src/pages/tab/category/index.vue
@@ -2,11 +2,11 @@
   <view class="flex" style="height: 100%;">
     <view class="w-180rpx">
       <view v-for="(gradeItem, gradeindex) in gradeList" :key="gradeindex" class="h-112rpx text-center line-height-112rpx" :style="currentGradeIndex === gradeindex ? 'background:white' : ''" @click="clickGrade(gradeindex)">
-        {{ gradeItem.gradeName }}
+        {{ gradeItem?.gradeName }}
       </view>
     </view>
     <view class="bg-white flex-1">
-      <view v-for="(semesterItem, semesterIndex) in gradeList[currentGradeIndex].semester" :key="semesterIndex" class="flex flex-col items-center">
+      <view v-for="(semesterItem, semesterIndex) in gradeList[currentGradeIndex]?.semester" :key="semesterIndex" class="flex flex-col items-center">
         <view class="mt-30rpx">
           {{ semesterItem.title }}
         </view>
@@ -16,9 +16,10 @@
             col="3"
           >
             <u-grid-item
-              v-for="(subjectItem, subjectIndex) in semesterItem.subjectCategory"
+              v-for="(subjectItem, subjectIndex) in semesterItem?.subjectCategory"
               :key="subjectIndex"
               style="margin-top: 16rpx;"
+              @click="clickSubject(gradeList[currentGradeIndex]?.gradeId, semesterItem?.semesterId, subjectItem?.subjectId)"
             >
               <!-- <image :src="subjectItem.icon"  width="80rpx" height="80rpx" class="h-120rpx w-120rpx" /> -->
               <image src="/static/images/home/logo.png" width="80rpx" height="80rpx" class="h-120rpx w-120rpx" />
@@ -51,6 +52,26 @@ onMounted(() => {
 onShow(() => {
   currentGradeIndex.value = commonStore.gradeIndex || 0;
 });
+
+const clickSubject = (gradeId: string, semesterId: string, subjectId: string) => {
+  uni.navigateTo({
+    url: '/pages/common/list/index',
+  });
+  wx.cloud.callFunction({
+    name: 'getFormationList',
+    data: {
+      gradeId,
+      semesterId,
+      subjectId,
+    },
+  })
+    .then((res) => {
+      console.log('res=>', res?.result?.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 /** 获取年级列表 */
 function getGradeList() {
